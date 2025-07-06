da3d02401dbb198bc22b1ca33830808a20e6f4cc
soumya <soumya24work@gmail.com>
2025-07-06 21:35:58 +0530

    User home screen UI created(not completed)

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+       {/* sample location data */}
+      <div className="flex items-center justify-start my-4 gap-3">
+        <h2 className="bg-[#eeeeee] h-10 w-16 rounded-full flex items-center justify-center">
+          <i className="font-semibold ri-map-pin-2-line"></i>
+        </h2>
+        <h4 className="font-medium">
+          Subhaspally (NEAR SYMA Club), Madhyamgram, Kolkata-700130
+        </h4>
+      </div>
+      <div className="flex items-center justify-start my-4 gap-3">
+        <h2 className="bg-[#eeeeee] h-10 w-16 rounded-full flex items-center justify-center">
+          <i className="font-semibold ri-map-pin-2-line"></i>
+        </h2>
+        <h4 className="font-medium">
+          Subhaspally (NEAR SYMA Club), Madhyamgram, Kolkata-700130
+        </h4>
+      </div>
+      <div className="flex items-center justify-start my-4 gap-3">
+        <h2 className="bg-[#eeeeee] h-10 w-16 rounded-full flex items-center justify-center">
+          <i className="font-semibold ri-map-pin-2-line"></i>
+        </h2>
+        <h4 className="font-medium">
+          Subhaspally (NEAR SYMA Club), Madhyamgram, Kolkata-700130
+        </h4>
+      </div>
+    </div>
+  );
+};
+
+export default LocationSearchPanel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,128 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useRef, useState } from "react";
+import "remixicon/fonts/remixicon.css";
+import { Link } from "react-router-dom";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import LocationSearchPanel from "../components/LocationSearchPanel";
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+  const [pickup, setPickup] = useState("");
+  const [destination, setDestination] = useState("");
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
+  const panelCloseRef = useRef(null);
 
-export default Home
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setPickup("");
+    setDestination("");
+  };
+
+  useGSAP(
+    function () {
+      if (panelOpen) {
+        gsap.to(panelRef.current, {
+          height: "70%",
+          paddingLeft: "24px",
+          duration: 0.5,
+          ease: "power2.inOut",
+        });
+        gsap.to(panelCloseRef.current, {
+          opacity: 1,
+          duration: 0.5,
+          ease: "power2.inOut",
+        });
+      } else {
+        gsap.to(panelRef.current, {
+          height: 0,
+          duration: 0.5,
+          ease: "power2.inOut",
+        });
+        gsap.to(panelCloseRef.current, {
+          opacity: 0,
+          duration: 0.5,
+          ease: "power2.inOut",
+        });
+      }
+    },
+    [panelOpen]
+  );
+
+  return (
+    <div className="h-screen relative">
+      <div className="flex justify-between">
+        <img
+          className="w-24 absolute px-4 py-5"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt=""
+        />
+      </div>
+      <div className="absolute top-4 right-4 text-2xl">
+        <i className="right-0 ri-user-fill"></i>
+      </div>
+
+      {/* temporary image */}
+      <div>
+        <img
+          className="h-screen w-screen "
+          src="https://imgs.search.brave.com/jK1glZ46aRnMBCp3Bqv7YWCuqyKXawBDiB0s1mPwES0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zaW1v/bnBhbi5jb20vd3At/Y29udGVudC90aGVt/ZXMvc3BfcG9ydGZv/bGlvL2Fzc2V0cy91/YmVyLW5vdC1waWNr/dXAuanBn"
+          alt=""
+        />
+      </div>
+
+      <div className="h-screen absolute top-0 w-full flex flex-col justify-end">
+        <div className="h-1/3 bg-white rounded-lg px-4 relative">
+          <div
+            ref={panelCloseRef}
+            onClick={() => {
+              setPanelOpen(false);
+            }}
+            className="absolute opacity-0 top-3 right-6 text-2xl"
+          >
+            <i className="ri-arrow-down-wide-line"></i>
+          </div>
+          <h4 className="font-bold text-2xl  py-3">Find a trip</h4>
+
+          <form
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+          >
+            <div className="line absolute h-20 w-1 top-[36%] left-9 rounded-full bg-gray-900"></div>
+            <input
+              value={pickup}
+              onChange={(e) => {
+                setPickup(e.target.value);
+              }}
+              onClick={() => {
+                setPanelOpen(true);
+              }}
+              className="bg-[#eeeeee] w-full px-12 py-3 mb-5 rounded border text-lg placeholder:text-base"
+              type="text"
+              placeholder="Add a pickup location"
+            />
+            <input
+              value={destination}
+              onChange={(e) => {
+                setDestination(e.target.value);
+              }}
+              onClick={() => {
+                setPanelOpen(true);
+              }}
+              className="bg-[#eeeeee] w-full px-12 py-3 mb-5 rounded border text-lg placeholder:text-base"
+              type="text"
+              placeholder="Enter your destination"
+            />
+          </form>
+        </div>
+
+        <div ref={panelRef} className="h-0 w-full bg-white">
+          <LocationSearchPanel />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
